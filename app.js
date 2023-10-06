6473bd3b26f49e9f7cf8150e330044265974c837
Dorian Kita <dorian.kita90@gmail.com>
2023-10-06 17:37:56 +0200

    created app structure, functions in app.js file

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 class DrawingApp {
     constructor(){
         this.canvas = document.getElementById('canvas');
+        this.painting = false;
         this.#init();
+        this.#initEvents();
     }
 
     #init(){
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
         this.ctx = this.canvas.getContext('2d');
+        this.ctx.strokeStyle = 'white';
+    }
+
+    #initEvents(){
+        //mouse methods
+
+        //touch methods
+    }
+
+    #startPosition(){
+        this.painting = !this.painting;
+        this.#draw();
+    }
+
+    #endPosition(){
+        this.painting = !this.painting;
+    }
+
+    #draw(){
+
+    }
+
+    changeColor(color){
+        this.ctx.strokeStyle = color;
     }
 }
 
